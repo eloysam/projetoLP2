5f3d9a4f80396d02ef2e174e5d37d8ef31154840
laura-LisboA <laurasantoa40@gmail.com>
2024-11-23 10:01:36 -0300

    .

--- projetolp2/src/main/java/com/ifba/projetolp2/repository/IEntradaRepository.java
+++ projetolp2/src/main/java/com/ifba/projetolp2/repository/IEntradaRepository.java
@@ -1,5 +1,8 @@
 package com.ifba.projetolp2.repository;
 
-public class IEntradaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ifba.projetolp2.model.Entrada;
+
+public interface IEntradaRepository extends JpaRepository<Entrada, Long> {
     
 }
