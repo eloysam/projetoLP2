70758ab96c39af0b1b8ae11481e995c3e56c1d5f
laura-LisboA <laurasantoa40@gmail.com>
2024-11-24 15:31:04 -0300

    main

--- projetolp2/src/main/java/com/ifba/projetolp2/Projetolp2Application.java
+++ projetolp2/src/main/java/com/ifba/projetolp2/Projetolp2Application.java
@@ -1,13 +1,31 @@
 package com.ifba.projetolp2;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import com.ifba.projetolp2.model.Venda;
+import com.ifba.projetolp2.repository.IVendaRepository;
 
 @SpringBootApplication
 public class Projetolp2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projetolp2Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner demo(IVendaRepository repVen) {
+		return (args) -> {
+
+			Venda v = new Venda();
+			v.setValorVenda(59.6);
+			repVen.save(v);
+
+			System.out.println(repVen.findAll());
+
+
+		};
 	}
 
 }
