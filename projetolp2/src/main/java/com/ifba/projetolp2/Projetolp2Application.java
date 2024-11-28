82645e309dcecdab90d5fff58580175874e261a0
Eloysa Ferreira Monteiro <114229411+eloysam@users.noreply.github.com>
2024-11-28 10:24:20 -0300

    commit

--- projetolp2/src/main/java/com/ifba/projetolp2/Projetolp2Application.java
+++ projetolp2/src/main/java/com/ifba/projetolp2/Projetolp2Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import com.ifba.projetolp2.model.Itens_entrada;
 import com.ifba.projetolp2.model.Venda;
 import com.ifba.projetolp2.repository.IVendaRepository;
+import com.ifba.projetolp2.repository.Iitens_entradaRepository;
 
 @SpringBootApplication
 public class Projetolp2Application {
@@ -15,7 +18,7 @@ public class Projetolp2Application {
 	}
 
 	@Bean
-	public CommandLineRunner demo(IVendaRepository repVen) {
+	public CommandLineRunner demo(IVendaRepository repVen, Iitens_entradaRepository itEnrep) {
 		return (args) -> {
 
 			Venda v = new Venda();
@@ -23,8 +26,16 @@ public class Projetolp2Application {
 			v.setIdVendedor(Long.valueOf(3));
 			v.setValorVenda(59.6);
 			v.setDataVenda("23/12/23");
-			//v.setVendaItens("shampoo", "sabonete");
+			//v.setVendaItens(null);
 			repVen.save(v);
+
+			Itens_entrada itEn = new Itens_entrada();
+			itEn.setEntrada(null);
+			itEn.setId(Long.valueOf(1));
+			itEn.setIdProduto(null);
+			itEn.setQuantidade(123);
+			itEn.setValorUnitario(12.20);
+			itEnrep.save(itEn);
 
 			System.out.println(repVen.findAll());
 
